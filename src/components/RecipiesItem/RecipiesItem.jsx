b09e21485ab1e372002bf97437e57b02158a0e27
kovalevska olha <146865293+KovalevskaOlga@users.noreply.github.com>
2025-09-14 10:58:15 +0300

    fix homepage

--- src/components/RecipiesItem/RecipiesItem.jsx
+++ src/components/RecipiesItem/RecipiesItem.jsx
@@ -1,7 +1,7 @@
 export const RecipiesItem = ({recipe: {name, time, servings,calories, difficulty, image}}) => {
     return (
         <li className="recipies__item">
-            <img className='recipies__img' src={image} alt={name}></img>
+            <img className='recipies__img' src={image} alt={name} />
             <h2 className='recipies__title'>{name}</h2>
             <p className='recipies__time'>Time: {time}</p>
             <p className='recipies__calories'>Calories: {calories}</p>
